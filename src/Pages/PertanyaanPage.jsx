03e723646badadfec6a77ab072f902f1d788a4b5
NdreNdre <andresahulata08@gmail.com>
2023-09-25 04:19:37 +0700

    first commit

---
+++ src/Pages/PertanyaanPage.jsx
@@ -0,0 +1,11 @@
+import FaqComponents from "../components/FaqComponents";
+
+const PertanyaanPage = () => {
+  return (
+    <div className=" faq-pages">
+        <FaqComponents />
+    </div>
+  )
+}
+
+export default PertanyaanPage
